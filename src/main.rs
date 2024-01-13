6e6dfffef9d58c914788517c3316c67fb49dbcfc
Piotr Wysocki <pan@piotrus.io>
2024-01-13 07:14:08 +0100

    tokio axum starter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, Costyrion!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = app();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(handler))
+}
+
+async fn handler() -> String {
+    "Costyrion!".to_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use tower::ServiceExt; // for `collect`
+
+    #[tokio::test]
+    async fn test_app() {
+        let app = app();
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"Costyrion!");
+    }
 }
