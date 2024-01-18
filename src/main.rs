0464ade0cd1c653e84c4b634b9cadfacc726f8d2
Piotr Wysocki <pan@piotrus.io>
2024-01-18 20:20:36 +0100

    resource crud with in-memory db

--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,152 @@
-use std::env;
-
-use axum::body::Body;
-use axum::http::Response;
 use axum::{
-    extract::Path,
+    error_handling::HandleErrorLayer,
+    extract::{Path, Query, State},
     http::StatusCode,
-    routing::{delete, get, post},
+    response::IntoResponse,
+    routing::{delete, get, patch},
     Json, Router,
 };
-use mongodb::{options::ClientOptions, Client};
 use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct Resource {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    id: Option<u64>,
-    name: String,
-}
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
-    let uri = env::var("MONGODB_URL").expect("set environment var: MONGODB_URL");
-    let mut options = ClientOptions::parse(&uri).await.unwrap();
-    options.app_name = Some("Costyrion".to_string());
-    let client = Client::with_options(options).unwrap(); // Unwrap the Result to get the Client instance
-    let db = client.database("costyrion");
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "costyrion=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let app = app();
+    let db = Db::default();
+    let app = app(db);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-fn app() -> Router {
+fn app(db: Db) -> Router {
     Router::new()
-        .route("/", get(handler))
-        .route("/resource", post(create_resource))
-        .route("/resource/:id", delete(delete_resource).get(get_resource))
+        .route("/resource", get(resource_index).post(resource_create))
+        .route(
+            "/resource/:id",
+            patch(resource_update).delete(resource_delete),
+        )
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {error}"),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        )
+        .with_state(db)
 }
 
-async fn handler() -> String {
-    "Costyrion!".to_owned()
+async fn resource_index(
+    pagination: Option<Query<Pagination>>,
+    State(db): State<Db>,
+) -> impl IntoResponse {
+    let resources = db.read().unwrap();
+    let Query(pagination) = pagination.unwrap_or_default();
+    let resources = resources
+        .values()
+        .skip(pagination.offset.unwrap_or(0))
+        .take(pagination.limit.unwrap_or(usize::MAX))
+        .cloned()
+        .collect::<Vec<_>>();
+    Json(resources)
 }
 
-async fn create_resource(Json(payload): Json<Resource>) -> (StatusCode, Json<Resource>) {
+async fn resource_create(
+    State(db): State<Db>,
+    Json(payload): Json<CreateResource>,
+) -> impl IntoResponse {
     let resource = Resource {
-        id: Some(1337),
+        id: Uuid::new_v4(),
         name: payload.name,
+        status: true,
     };
 
-    (StatusCode::CREATED, Json(resource))
+    db.write().unwrap().insert(resource.id, resource.clone());
 }
 
-async fn delete_resource(Path(id): Path<u64>) -> Result<Response<Body>, StatusCode> {
-    let message = format!("Resource with id: {} deleted successfully", id);
-    Ok(Response::new(Body::from(message)))
-}
+async fn resource_update(
+    Path(id): Path<Uuid>,
+    State(db): State<Db>,
+    Json(input): Json<UpdateResource>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let mut resource = db
+        .read()
+        .unwrap()
+        .get(&id)
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
 
-async fn get_resource(Path(id): Path<u64>) -> Result<Json<Resource>, StatusCode> {
-    let resource = Resource {
-        id: Some(id),
-        name: "Costyrion".to_string(),
-    };
+    if let Some(name) = input.name {
+        resource.name = name;
+    }
+
+    if let Some(status) = input.status {
+        resource.status = status;
+    }
+
+    db.write().unwrap().insert(resource.id, resource.clone());
 
     Ok(Json(resource))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use axum::{
-        body::Body,
-        http::{Request, StatusCode},
-    };
-    use http_body_util::BodyExt;
-    use tower::ServiceExt; // for `collect`
-
-    #[tokio::test]
-    async fn test_app() {
-        let app = app();
-
-        let response = app
-            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
-            .await
-            .unwrap();
-
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let body = response.into_body().collect().await.unwrap().to_bytes();
-        assert_eq!(&body[..], b"Costyrion!");
+async fn resource_delete(
+    Path(id): Path<Uuid>,
+    State(db): State<Db>,
+) -> impl IntoResponse {
+    if db.write().unwrap().remove(&id).is_some() {
+        StatusCode::NO_CONTENT
+    } else {
+        StatusCode::NOT_FOUND
     }
+}
 
-    #[tokio::test]
-    async fn test_create_resource() {
-        let app = app();
-        let resource = Resource {
-            id: None,
-            name: "Test Resource".to_owned(),
-        };
-        let payload = serde_json::to_vec(&resource).unwrap();
+type Db = Arc<RwLock<HashMap<Uuid, Resource>>>;
 
-        let request = Request::builder()
-            .method("POST")
-            .uri("/resource")
-            .header("content-type", "application/json")
-            .body(Body::from(payload))
-            .unwrap();
+#[derive(Debug, Serialize, Clone)]
+struct Resource {
+    id: Uuid,
+    name: String,
+    status: bool,
+}
 
-        let response = app.oneshot(request).await.unwrap();
+#[derive(Debug, Deserialize)]
+struct CreateResource {
+    name: String,
+}
 
-        assert_eq!(response.status(), StatusCode::CREATED);
+#[derive(Debug, Deserialize)]
+struct UpdateResource {
+    name: Option<String>,
+    status: Option<bool>,
+}
 
-        let body = response.into_body().collect().await.unwrap().to_bytes();
-        let returned_resource: Resource = serde_json::from_slice(&body).unwrap();
-
-        assert_eq!(returned_resource.id, Some(1337));
-        assert_eq!(returned_resource.name, resource.name);
-    }
-
-    #[tokio::test]
-    async fn test_delete_resource() {
-        let app = app();
-        let id = 1337;
-
-        let request = Request::builder()
-            .method("DELETE")
-            .uri(format!("/resource/{}", id))
-            .body(Body::empty())
-            .unwrap();
-
-        let response = app.oneshot(request).await.unwrap();
-
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let body = response.into_body().collect().await.unwrap().to_bytes();
-        let message = std::str::from_utf8(&body).unwrap();
-        let expected_message = format!("Resource with id: {} deleted successfully", id);
-
-        assert_eq!(message, expected_message);
-    }
-
-    #[tokio::test]
-    async fn test_get_resource() {
-        let app = app();
-        let id = 1337;
-
-        let request = Request::builder()
-            .method("GET")
-            .uri(format!("/resource/{}", id))
-            .body(Body::empty())
-            .unwrap();
-
-        let response = app.oneshot(request).await.unwrap();
-
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let body = response.into_body().collect().await.unwrap().to_bytes();
-        let returned_resource: Resource = serde_json::from_slice(&body).unwrap();
-
-        assert_eq!(returned_resource.id, Some(id));
-        assert_eq!(returned_resource.name, "Costyrion");
-    }
+#[derive(Debug, Deserialize, Default)]
+pub struct Pagination {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
 }
